b633fa3ce076c04b485c39ec3fa0a03524b23343
GarethW85 <tidylad@hotmail.com>
2022-06-14 18:37:00 +0200

    Rainbow Challenge JS
    Onclick function to display colors

---
+++ rainbox.js
@@ -0,0 +1,41 @@
+//RAINBOW CHALLENGE
+
+//STEP 1 : Create the function
+//STEP 2 : Create the variable inside the the function that will target the ID tags
+//STEP 3 : variableName.style.backgroundcolor = "the color you want the background to be";
+
+
+function redStyle(){
+    var element = document.getElementById("red");
+    element.style.backgroundColor = "red";
+}
+
+function orangeStyle(){
+    var element2 = document.getElementById("orange");
+    element2.style.backgroundColor = "orange";
+}
+
+function yellowStyle(){
+    var element3 = document.getElementById("yellow");
+    element3.style.backgroundColor = "yellow";
+}
+
+function greenStyle(){
+    var element4 = document.getElementById("green");
+    element4.style.backgroundColor = "green";
+}
+
+function blueStyle(){
+    var element5 = document.getElementById("blue");
+    element5.style.backgroundColor = "blue";
+}
+
+function purpleStyle(){
+    var element6 = document.getElementById("darkPurple");
+    element6.style.backgroundColor = "purple";
+}
+
+function pinkStyle(){
+    var element7 = document.getElementById("purple");
+    element7.style.backgroundColor = "salmon";
+}
